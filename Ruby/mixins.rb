a5258a68a963678f98d0cda19bb18ecbecac9415
keakalamudo <kelo.akalamudo@gmail.com>
2014-09-24 01:10:57 -0700

    mixins file update

--- Ruby/mixins.rb
+++ Ruby/mixins.rb
@@ -14,3 +14,72 @@ using the usual <, >, etc. operators.  Do not use <=> in your test code.
 If your class inherits from another class then the module mixin avoids the
 problem of multiple inheritance.
 =end
+
+class Mixins
+  include Comparable
+  attr :int
+  def <=>(anOther)
+    divisible(int) <=> anOther.divisible(anOther.int)
+  end
+  def initialize(int)
+    @int = int
+  end
+  def inspect
+    @int
+  end
+
+  def divisible(n)
+    count =0
+    while(n%2==0)
+        n = n/2
+        count = count +1
+    end
+    return count
+  end
+end
+
+s1 = Mixins.new(4)
+s2 = Mixins.new(10)
+s3 = Mixins.new(64)
+
+print "Is s1 > s2: "
+puts s1 > s2
+
+print "Is s1 < s2: "
+puts s1 < s2
+
+print "Is s1 == s2: "
+puts s1 == s2
+
+print "Is s1 <= s2: "
+puts s1 <= s2
+
+print "Is s1 > s2: "
+puts s1 > s2
+
+print "Is s3 < s2: "
+puts s3 < s2
+
+print "Is s1 == s3: "
+puts s1 == s3
+
+print "Is s3 <= s2: "
+puts s3 <= s2
+
+print "Is s3 > s2: "
+puts s3 > s2
+
+print "Is s3 < s2: "
+puts s3 < s2
+
+print "Is s3 == s2: "
+puts s3 == s2
+
+print "Is s1 between s2 and s3: "
+puts s1.between?(s2,s3)
+
+print "Is s3 between s1 and s2: "
+puts s3.between?(s1,s2)
+
+print "Is s2 between s1 and s3: "
+puts s2.between?(s1,s3)
